6422cd6e846335e2a8f42d976f8e79917cd6be2b
“rtyagi089” <rtyagi@xebia.com>
2017-07-10 12:04:30 +0530

    Initial Commit

---
+++ DragAndDrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragAndDrop
+//
+//  Created by Ravi Tyagi on 10/07/17.
+//  Copyright © 2017 Xebia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
